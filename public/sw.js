fe50046ce0500cf68434860ef9aa481b7108696a
Aidil Safwan <edelamrl@gmail.com>
2026-08-25 22:11:38 +0800

    Refresh stale PWA app shell on deploy

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,10 @@
-const CACHE = 'amanda-piggy-bank-v1'
-self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(['/','/manifest.webmanifest']))))
-self.addEventListener('fetch', (event) => event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => { const copy = response.clone(); caches.open(CACHE).then((cache) => cache.put(event.request, copy)); return response }).catch(() => cached))))
+const CACHE = 'amanda-piggy-bank-v2'
+self.addEventListener('install', (event) => event.waitUntil(self.skipWaiting()))
+self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())))
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(fetch(event.request).then((response) => { const copy = response.clone(); caches.open(CACHE).then((cache) => cache.put(event.request, copy)); return response }).catch(() => caches.match(event.request)))
+    return
+  }
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => { const copy = response.clone(); caches.open(CACHE).then((cache) => cache.put(event.request, copy)); return response }).catch(() => cached)))
+})
